17c7fdfd6e5b202960d16e178e432777a08e3e37
Omar Farouk <fomar9235@gmail.com>
2025-07-03 11:13:43 +0100

    Ajout de la page d'onboarding avec animations et intégration de la dépendance animate_do

---
+++ lib/screens/onboarding_page.dart
@@ -0,0 +1,46 @@
+import "package:flutter/material.dart";
+import "package:animate_do/animate_do.dart";
+import "package:pharma_niamey/models/onboarding_model.dart";
+
+class OnboardingPage extends StatelessWidget {
+  final OnBoardingModel page;
+  const OnboardingPage({super.key, required this.page});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElasticInRight(
+            child: Image.asset(
+              page.imageUrl,
+              width: double.infinity,
+              height: _size.width * 0.9,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Text(
+            page.titlePage,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          FadeIn(
+            child: Text(
+              page.descriptionText,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
